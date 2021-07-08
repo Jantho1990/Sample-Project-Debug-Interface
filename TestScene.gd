56140c8e98bfdb5754ccdf6ed5888128f543eaba
Josh Anthony <jantho1990@gmail.com>
2021-07-07 21:18:40 -0500

    tutorial part 3

--- TestScene.gd
+++ TestScene.gd
@@ -7,6 +7,8 @@ func _process(_delta) -> void:
   if test_ct >= 1000:
     test_ct = -1
   elif test_ct == 900:
-    Debug.update_widget('TextList1.remove_label', { 'name': 'counter' })
+    Debug.update_widget('TestDebugContainer:TextList1.remove_label', { 'name': 'counter' })
+    Debug.update_widget('TestDebugContainer2:TextList1.remove_label', { 'name': 'counter' })
   elif test_ct < 900:
-    Debug.update_widget('TextList1.add_label', { 'name': 'counter', 'value': str(test_ct) })
+    Debug.update_widget('TestDebugContainer:TextList1.add_label', { 'name': 'counter', 'value': str(test_ct) })
+    Debug.update_widget('TestDebugContainer2:TextList1.add_label', { 'name': 'counter', 'value': str(round(test_ct / 10)) })
--- _debug/DebugLayer.gd
+++ _debug/DebugLayer.gd
@@ -1,50 +1,43 @@
 extends CanvasLayer
 
+signal debug_container_registered
+
 
 # Boolean that governs whether the debug interface should be visible.
 var show_debug_interface = false
 
-# The list of widget keywords associated with the DebugLayer.
-var _widget_keywords = {}
+# The debug containers registered to the DebugLayer.
+var _debug_containers = {}
+
+# The currently active debug container.
+var _debugContainer: Node
 
 # Container for the UI that we'll show and hide.
 onready var _uiContainer = $DebugUIContainer
 
+# Nodes implementing the debug container tab switching interface.
+onready var debugTabs = $DebugUIContainer/VBoxContainer/DebugTabs
+onready var debugContentContainer = $DebugUIContainer/VBoxContainer/DebugContentContainer
+
 
 func _ready() -> void:
   _set_ui_container_visibility(show_debug_interface)
-  _register_debug_widgets(self)
+  debugTabs.connect('tab_changed', self, '_on_Tab_changed')
 
 
 func _input(_event) -> void:
   if Input.is_action_just_pressed('toggle_debug_interface'):
     show_debug_interface = !show_debug_interface
-    _set_ui_container_visibility(show_debug_interface)
+    _set_ui_container_visibility(show_debug_interface)  
+    _debugContainer.show()
 
 
-# Adds a widget keyword to the registry.
-func _add_widget_keyword(widget_keyword: String, widget_node: Node) -> void:
-  var widget_node_name = widget_node.name if 'name' in widget_node else str(widget_node)
-
-  if not _widget_keywords.has(widget_node_name):
-    _widget_keywords[widget_node_name] = {}
-
-  if not _widget_keywords[widget_node_name].has(widget_keyword):
-    _widget_keywords[widget_node_name][widget_keyword] = widget_node
-  else:
-    var widget = _widget_keywords[widget_node_name][widget_keyword]
-    var widget_name = widget.name if 'name' in widget else str(widget)
-    push_error('DebugLayer._add_widget_keyword(): Widget keyword "' + widget_node_name + '.' + widget_keyword + '" already exists (' + widget_name + ')')
-    return
-
-
-# Go through all children of provided node and register any DebugWidgets found.
-func _register_debug_widgets(node) -> void:
-  for child in node.get_children():
-    if child is DebugWidget:
-      register_debug_widget(child)
-    elif child.get_child_count() > 0:
-      _register_debug_widgets(child)
+func _on_Tab_changed(tab_index) -> void:
+  var tab_name = debugTabs.get_tab_title(tab_index)
+  var containerNode = _debug_containers[tab_name]
+  _debugContainer.hide()
+  _debugContainer = containerNode
+  _debugContainer.show()
 
 
 # Sets the visibility of the UI container.
@@ -52,22 +45,40 @@ func _set_ui_container_visibility(boolean) -> void:
   _uiContainer.visible = boolean
 
 
-# Register a single DebugWidget to the DebugLayer.
-func register_debug_widget(widgetNode) -> void:
-  for widget_keyword in widgetNode.get_widget_keywords():
-    _add_widget_keyword(widget_keyword, widgetNode)
+func register_debug_container(containerNode) -> void:
+  var container_name = containerNode.name
+  if _debug_containers.has(container_name):
+    push_error('DebugLayer.register_debug_container: Debug already has registered DebugContainer with name "' + container_name + '".')
+    return
+
+  # Reparent the container node to the DebugLayer.
+  containerNode.get_parent().call_deferred('remove_child', containerNode)
+  debugContentContainer.call_deferred('add_child', containerNode)
+  debugTabs.add_tab(container_name)
+
+  _debug_containers[container_name] = containerNode
+  if _debug_containers.size() == 1:
+    _debugContainer = containerNode
+
+  # Hide this container node so we don't show debug info by default.
+  containerNode.hide()
+
+  emit_signal('debug_container_registered', containerNode)
 
 
-# Sends data to the widget with widget_name, triggering the callback for widget_keyword.
-func update_widget(widget_path: String, data) -> void:
-  var split_widget_path = widget_path.split('.')
-  if split_widget_path.size() == 1 or split_widget_path.size() > 2:
-    push_error('DebugContainer.update_widget(): widget_path formatted incorrectly. ("' + widget_path + '")')
+# Sends data to the debug container specified in widget_path.
+# API: container_name:widget_name.widget_keyword
+func update_widget(widget_path: String, data = null) -> void:
+  var split_keyword = widget_path.split(':')
+  if split_keyword.size() == 1:
+    push_error('DebugLayer.update_widget(): No container name was specified. (' + widget_path + ', ' + str(data) + ')')
+    return
 
-  var widget_name = split_widget_path[0]
-  var widget_keyword = split_widget_path[1]
+  var container_name = split_keyword[0]
+  if not _debug_containers.has(container_name):
+    push_error('DebugLayer.update_widget(): Container with name "' + container_name + '" is not registered.')
+    return
 
-  if _widget_keywords.has(widget_name) and _widget_keywords[widget_name].has(widget_keyword):
-    _widget_keywords[widget_name][widget_keyword].handle_callback(widget_keyword, data)
-  else:
-    push_error('DebugContainer.update_widget(): Widget name and keyword "' + widget_name + '.' + widget_keyword  + '" not found (' + str(_widget_keywords) + ')')
+  var containerNode = _debug_containers[container_name]
+  widget_path = split_keyword[1]
+  containerNode.update_widget(widget_path, data)
